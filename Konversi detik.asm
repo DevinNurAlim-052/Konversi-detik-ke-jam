b1e4e62a6a042ef0930f58cfe98bdec5aec79ea6
Devin Nur Alim - 2300018052 <154082355+DevinNuralim052@users.noreply.github.com>
2023-12-22 04:30:02 +0700

    Add files via upload

---
+++ Konversi detik.asm
@@ -0,0 +1,80 @@
+.model small
+.stack 100h
+
+.data
+detik dw 3665
+jam db 0 
+menit db 0
+detik_sisa dw 0
+pembagi dw 3600  
+
+.code
+main proc
+    mov ax, @data
+    mov ds, ax
+    
+    mov ax, detik
+    mov dx, 0
+    mov bx, pembagi 
+    div bx
+    mov jam, al
+    
+    mov ax, dx
+    mov dx, 0
+    mov bx, 60
+    div bx
+    mov menit, al
+    mov detik_sisa, dx
+    
+    mov ah, 2
+    
+    ; Menampilkan JAM
+    add jam, 30h
+    mov dl, jam
+    int 21h
+    mov dl, 'j'
+    int 21h
+    mov dl, 'a'
+    int 21h
+    mov dl, 'm'
+    int 21h   
+    
+    
+    ; Menampilkan MENIT
+    mov dl, ' '
+    int 21h 
+    add menit, 30h
+    mov dl, menit
+    int 21h
+    mov dl, 'm'
+    int 21h
+    mov dl, 'e'
+    int 21h
+    mov dl, 'n'
+    int 21h
+    mov dl, 'i'
+    int 21h
+    mov dl, 't'
+    int 21h
+    
+    ; Menampilkan DETIK
+    mov dl, ' '
+    int 21h
+    add detik_sisa, 30h
+    mov dx, detik_sisa
+    int 21h
+    mov dl, 'd'
+    int 21h
+    mov dl, 'e'
+    int 21h
+    mov dl, 't'
+    int 21h
+    mov dl, 'i'
+    int 21h
+    mov dl, 'k'
+    int 21h
+          
+    mov ax, 4c00h
+    int 21h
+main endp
+end main
